9a3d90b37c8e853e7d302138294bc52cd8ba6650
Kyle Liang <kyleliang@Kyles-MacBook-Pro.local>
2017-11-25 16:06:17 +0800

    finished user page with placeholder data

--- src/pages/user/UserContainer.js
+++ src/pages/user/UserContainer.js
@@ -13,9 +13,59 @@ export default class UserContainer extends Component {
     }
 
     render() {
+        var total = 20
+        var accepted = 16
+        var rejected = 4
+        var acceptanceRate = accepted/total
+        var username="placeholder1"
+        var email="placeholder@placeholder.com"
         return (
-            <div>
-                <p>User Profile page</p>
+          <div>
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+              <a class="navbar-brand" href="#">Page Title</a>
+              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                  <li class="nav-item">
+                    <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Features</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Sign Out</a>
+                  </li>
+                </ul>
+              </div>
+            </nav>
+              <div className = "body">
+                <div class = "userInfo">
+                  <h3> Username: {username} </h3>
+                  <h3> Email: {email} </h3>
+                </div>
+                <div class = "ele">
+                  <h2> Total Submissions </h2>
+                  <br/>
+                  <h3> {total} </h3>
+                </div>
+                <div class = "ele">
+                  <h2> Accepted Submissions </h2>
+                  <br/>
+                  <h3> {accepted} </h3>
+                </div>
+                <div class = "ele">
+                  <h2> Rejected Submissions </h2>
+                  <br/>
+                  <h3> {rejected} </h3>
+                </div>
+                <div class = "ele">
+                  <h2> Acceptance Rate </h2>
+                  <br/>
+                  <h3> {100*acceptanceRate} % </h3>
+                </div>
+              </div>
             </div>
         )
     }
